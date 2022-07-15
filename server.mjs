3b8b53d93e52377bde50905c96b6de12578b107f
fviscuso <viscuso.fabio@outlook.it>
2022-07-15 18:35:03 +0200

    feat: Send JSON from Mars

--- server.mjs
+++ server.mjs
@@ -4,8 +4,8 @@ import clc from "cli-color"
 const server = createServer((req, res) => {
     console.log("request received");
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end('<html><body><h1>Scemo chi legge</h1></body></html>');
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ location: 'Mars', message: 'Hello from another planet!' }));
 })
 
 server.listen(8080, () => { console.log(clc.bgGreenBright('server started at http://localhost:8080')) })
